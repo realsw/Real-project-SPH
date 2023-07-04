e4558cdce244637b6324e68be617050e2c32c2c0
realsw <shanwei.zhong@foxmail.com>
2023-07-04 15:45:21 +0800

    search模块根据不同参数获取数据展示

--- src/store/search/index.js
+++ src/store/search/index.js
@@ -22,7 +22,21 @@ const actions = {
 
 };
 //计算属性，在项目当中，为了简化数据而生。
-const getters = {};
+//在这里将之后需要在组件中使用的数据进行简化，方便之后在组件中获取数据
+const getters = {
+    //此处形参state为当前仓库中的state，并非大仓库中的state
+    goodsList(state){
+        //如果服务器数据回来了，没问题，是一个数组
+        //但需要考虑网络不好时，会返回undefined然后被遍历，所以需要||[],以防万一
+        return state.searchlist.goodsList||[];
+    },
+    trademarkList(state){
+        return state.searchlist.trademarkList||[];
+    },
+    attrsList(state){
+        return state.searchlist.attrsList||[];
+    },
+};
 
 export default {
     state,
